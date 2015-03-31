62b9e2a1f57149b777f875474b97e5a91bc4e1f8
jmallinga <jmallinga@gmail.com>
2015-03-31 09:49:34 -0400

    Creating a view template using the RazorViewEngine
    changed the Index method to return a View object

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -9,17 +9,17 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     {
         // GET: HelloWorld
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
+            return View();
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int numTimes = 1)
+        public string Welcome(string name, int ID = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes); 
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID); 
         }
     }
 }
